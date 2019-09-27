58cc5e4f1fc603ab67aa3d1572670ba5a5288ed8
omtamal <omtamal@gmail.com>
2019-09-27 20:38:25 +0200

    c

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -68,12 +68,12 @@ function updateGradient()
   var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
   var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
   var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-  var color1 = "rgb("+r1+","+g1+","+b1+","+0.3+")";
+  var color1 = "rgb("+r1+","+g1+","+b1+","+0.36+")";
 
   var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
   var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
   var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-  var color2 = "rgb("+r2+","+g2+","+b2+","+0.3+")";
+  var color2 = "rgb("+r2+","+g2+","+b2+","+0.36+")";
 
  $('#gradient').css({
    background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
